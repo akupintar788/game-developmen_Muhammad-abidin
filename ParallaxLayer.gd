7d8ae877b338edd3a986bfaa0245eae48ae5e887
akupintar788 <127075293+akupintar788@users.noreply.github.com>
2023-09-12 19:27:56 +0700

    muhammad abidin

---
+++ ParallaxLayer.gd
@@ -0,0 +1,11 @@
+extends ParallaxLayer
+
+@export var cloudspeed: float = -10
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass  # Ganti dengan kode yang sesuai.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	motion_offset.x = cloudspeed * delta
---
+++ ParallaxLayer3.gd
@@ -0,0 +1,8 @@
+extends ParallaxLayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass  # Ganti dengan kode yang sesuai.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
